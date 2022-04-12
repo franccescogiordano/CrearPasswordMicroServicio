cd67e935863159491925ea58bbd9aa6f00ddd876
francheskito <franccesco.giordano11@gmail.com>
2022-04-12 13:59:44 -0300

    creaando microservicio

---
+++ src/main/java/co/com/sofka/primerMicroServicio/PrimerMicroServicioApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.primerMicroServicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerMicroServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerMicroServicioApplication.class, args);
+	}
+
+}
